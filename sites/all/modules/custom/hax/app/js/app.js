1b41106c7478aee7aafee950abd30ec0e13d18cd
Michael Potter <mgp140@psu.edu>
2015-02-01 13:39:35 -0500

    Started UX app.

---
+++ sites/all/modules/custom/hax/app/js/app.js
@@ -0,0 +1,11 @@
+$(document).foundation(
+
+
+    // SVG / PNG
+//   if(!Modernizr.svg) {
+//     $('img[src*="svg"]').attr('src', function () {
+//     return $(this).attr('src').replace('.svg', '.png');
+//   });
+// }
+	
+);
